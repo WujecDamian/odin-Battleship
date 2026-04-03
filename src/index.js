14d370c3b81195beb9965db26262e66ae6ed4348
WujecDamian <wujecdamian12@gmail.com>
2026-04-03 10:14:41 +0200

    feat: Add imports for gameBoard, realPlayer, and aiPlayer in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 //put inside src/index.js
-import './styles.css'
+import "./styles.css";
+import { gameBoard } from "./scripts/gameBoard/gameBoard.js";
+import { realPlayer } from "./scripts/player/realPlayer.js";
+import { aiPlayer } from "./scripts/player/aiPlayer.js";
