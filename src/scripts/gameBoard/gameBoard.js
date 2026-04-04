e947a646a98f7b12a4df106a595908bea6fd73be
WujecDamian <wujecdamian12@gmail.com>
2026-04-04 08:34:18 +0200

    feat: Update gameBoard to initialize board with 10 elements and add receiveAttack method (pseudocode)

--- src/scripts/gameBoard/gameBoard.js
+++ src/scripts/gameBoard/gameBoard.js
@@ -1,8 +1,13 @@
 export function gameBoard() {
   return {
-    board: new Array(100).fill("x", 0, -1),
+    board: new Array(10).fill("[],[],[],[],[],[],[],[],[],[]", 0, -1),
     missed: 0,
     totalHits: 0,
-    receiveAttack(coordinates) {},
+    receiveAttack(coordinates) {
+      //Determine whether attack hit a ship
+      // t: => call hit function on correct ship [o]
+      // f: => mark spot as missed [x]
+    },
+    placeShip(ship, coordinates) {},
   };
 }
