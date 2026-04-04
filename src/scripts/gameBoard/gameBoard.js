dfacefd7a71b24f875b91a0eafa528f20e0676ca
WujecDamian <wujecdamian12@gmail.com>
2026-04-04 10:00:26 +0200

    feat: Enhance gameBoard with ship initialization and update placeShip method structure

--- src/scripts/gameBoard/gameBoard.js
+++ src/scripts/gameBoard/gameBoard.js
@@ -1,8 +1,17 @@
+import { shipObj } from "../ships/shipObj";
+
 export function gameBoard() {
   return {
     board: Array(10)
       .fill()
       .map(() => Array(10).fill("")),
+    ships: [
+      shipObj("carrier", 5),
+      shipObj("battleship", 4),
+      shipObj("cruiser", 3),
+      shipObj("submarine", 3),
+      shipObj("destroyer", 2),
+    ],
     missed: 0,
     totalHits: 0,
     receiveAttack(y, x) {
@@ -13,7 +22,29 @@ export function gameBoard() {
         this.board[y][x] = "x";
       }
     },
-    placeShip(ship, x, y) {},
+    placeShip(ship, orientation, y, x) {
+      let length = 0;
+      switch (String.toLowerCase(ship)) {
+        case "carrier":
+          length = 5;
+          break;
+        case "battleship":
+          length = 4;
+          break;
+        case "cruiser":
+        case "submarine":
+          length = 3;
+          break;
+        case "destroyer":
+          length = 2;
+          break;
+        default:
+          alert("Ship is invalid! There's nothing like - " + ship);
+      }
+      if (orientation === "horizontal") {
+      } else if (orientation === "vertical") {
+      }
+    },
     renderBoard() {
       for (let i = 0; i < this.board.length; i++) {
         let arrayString = "";
