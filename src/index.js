2caaea863beb194d8f9d26dec6df7054432b548b
WujecDamian <wujecdamian12@gmail.com>
2026-04-04 09:10:46 +0200

    feat: Initialize gameBoard instance and render it to the console

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import "./styles.css";
 import { gameBoard } from "./scripts/gameBoard/gameBoard.js";
 import { realPlayer } from "./scripts/player/realPlayer.js";
 import { aiPlayer } from "./scripts/player/aiPlayer.js";
+
+let gameBoard1 = gameBoard();
+
+gameBoard1.renderBoard();
