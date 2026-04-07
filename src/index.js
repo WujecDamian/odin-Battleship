41329fd3707ec2ba1e88316518afe865392ea853
WujecDamian <wujecdamian12@gmail.com>
2026-04-07 16:10:39 +0200

    feat(index.js): import ui module

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import { gameBoard } from "./scripts/gameBoard/gameBoard.js";
 import { realPlayer } from "./scripts/player/realPlayer.js";
 import { aiPlayer } from "./scripts/player/aiPlayer.js";
-
+import { ui } from "./scripts/DOM/ui.js";
 let gameBoard1 = gameBoard();
 
 gameBoard1.renderBoard();
