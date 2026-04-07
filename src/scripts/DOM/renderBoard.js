2ca1f22ffe7cf53cba9b468a5517a11fc5d4a64a
WujecDamian <wujecdamian12@gmail.com>
2026-04-07 21:28:06 +0200

    feat: add col & row id's for field elements in renderBoard

--- src/scripts/DOM/renderBoard.js
+++ src/scripts/DOM/renderBoard.js
@@ -1,11 +1,16 @@
 export function renderBoard(board, boardClass) {
   let boardDiv = document.querySelector(`.${boardClass}`);
   boardDiv.replaceChildren();
+  let col = 0;
   board.forEach((element) => {
+    let row = 0;
     element.forEach((field) => {
       let square = document.createElement("div");
       square.className = "square";
+      square.id = `${col},${row}`;
       boardDiv.append(square);
+      row++;
     });
+    col++;
   });
 }
