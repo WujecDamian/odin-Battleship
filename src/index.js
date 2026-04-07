a6624d5cfde2329a45c1e266caef351976c3e9d4
WujecDamian <wujecdamian12@gmail.com>
2026-04-07 21:08:19 +0200

    refactor: remove unused(non existent) renderBoard call from index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,3 @@ import { realPlayer } from "./scripts/player/realPlayer.js";
 import { aiPlayer } from "./scripts/player/aiPlayer.js";
 import { ui } from "./scripts/DOM/ui.js";
 let gameBoard1 = gameBoard();
-
-gameBoard1.renderBoard();
