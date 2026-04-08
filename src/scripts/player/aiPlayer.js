7e7d2fa2abb0ab2e2054b78f0eadd13636177c57
WujecDamian <wujecdamian12@gmail.com>
2026-04-08 11:42:17 +0200

    feat: add hasPlaced property and if statements to place all 5 ships correctly

--- src/scripts/player/aiPlayer.js
+++ src/scripts/player/aiPlayer.js
@@ -4,10 +4,9 @@ export function aiPlayer() {
   return {
     playerBoard: gameBoard(),
     makeMove() {},
-    placeShips() {
-      let ships = this.playerBoard.ships;
+    placeShips(ships) {
       ships.forEach((ship) => {
-        let placedCorrectly = false;
+        let hasPlaced = false;
         let orientations = ["horizontal", "vertical"];
         let pickedOrientation;
         function pickOrientation() {
@@ -16,21 +15,26 @@ export function aiPlayer() {
         pickOrientation();
 
         if (pickedOrientation === "horizontal") {
-          this.playerBoard.placeShip(
-            ship,
-            pickedOrientation,
-            Math.floor(Math.floor(Math.random() * 9)),
-            Math.floor(Math.floor(Math.random() * ship.lengthShip)),
-          );
+          while (hasPlaced === false) {
+            hasPlaced = this.playerBoard.placeShip(
+              ship,
+              pickedOrientation,
+              Math.floor(Math.floor(Math.random() * 9)),
+              Math.floor(Math.floor(Math.random() * ship.lengthShip)),
+            );
+          }
         } else if (pickedOrientation === "vertical") {
-          this.playerBoard.placeShip(
-            ship,
-            pickedOrientation,
-            Math.floor(
-              Math.floor(Math.random() * 9 - ship.lengthShip) + ship.lengthShip,
-            ),
-            Math.floor(Math.floor(Math.random() * 9)),
-          );
+          while (hasPlaced === false) {
+            hasPlaced = this.playerBoard.placeShip(
+              ship,
+              pickedOrientation,
+              Math.floor(
+                Math.floor(Math.random() * 9 - ship.lengthShip) +
+                  ship.lengthShip,
+              ),
+              Math.floor(Math.floor(Math.random() * 9)),
+            );
+          }
         }
       });
     },
