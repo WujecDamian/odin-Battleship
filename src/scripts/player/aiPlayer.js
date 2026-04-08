a2196b9714ccc908976722d1be8af72fc9a98023
WujecDamian <wujecdamian12@gmail.com>
2026-04-08 09:14:00 +0200

    feat: add placeShips function to ai player (for further rework)

--- src/scripts/player/aiPlayer.js
+++ src/scripts/player/aiPlayer.js
@@ -4,5 +4,35 @@ export function aiPlayer() {
   return {
     playerBoard: gameBoard(),
     makeMove() {},
+    placeShips() {
+      let ships = this.playerBoard.ships;
+      ships.forEach((ship) => {
+        let placedCorrectly = false;
+        let orientations = ["horizontal", "vertical"];
+        let pickedOrientation;
+        function pickOrientation() {
+          pickedOrientation = orientations[Math.floor(Math.random() * 2)];
+        }
+        pickOrientation();
+
+        if (pickedOrientation === "horizontal") {
+          this.playerBoard.placeShip(
+            ship,
+            pickedOrientation,
+            Math.floor(Math.floor(Math.random() * 9)),
+            Math.floor(Math.floor(Math.random() * ship.lengthShip)),
+          );
+        } else if (pickedOrientation === "vertical") {
+          this.playerBoard.placeShip(
+            ship,
+            pickedOrientation,
+            Math.floor(
+              Math.floor(Math.random() * 9 - ship.lengthShip) + ship.lengthShip,
+            ),
+            Math.floor(Math.floor(Math.random() * 9)),
+          );
+        }
+      });
+    },
   };
 }
