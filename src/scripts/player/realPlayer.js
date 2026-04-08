6813ef82e52998d5d26459724c72e55020f00aae
WujecDamian <wujecdamian12@gmail.com>
2026-04-08 12:24:16 +0200

    feat(realPlayer): add placesShips function to Real player

--- src/scripts/player/realPlayer.js
+++ src/scripts/player/realPlayer.js
@@ -3,5 +3,39 @@ import { gameBoard } from "../gameBoard/gameBoard.js";
 export function realPlayer() {
   return {
     playerBoard: gameBoard(),
+    placeShips(ships) {
+      ships.forEach((ship) => {
+        let hasPlaced = false;
+        let orientations = ["horizontal", "vertical"];
+        let pickedOrientation;
+        function pickOrientation() {
+          pickedOrientation = orientations[Math.floor(Math.random() * 2)];
+        }
+        pickOrientation();
+
+        if (pickedOrientation === "horizontal") {
+          while (hasPlaced === false) {
+            hasPlaced = this.playerBoard.placeShip(
+              ship,
+              pickedOrientation,
+              Math.floor(Math.floor(Math.random() * 9)),
+              Math.floor(Math.floor(Math.random() * ship.lengthShip)),
+            );
+          }
+        } else if (pickedOrientation === "vertical") {
+          while (hasPlaced === false) {
+            hasPlaced = this.playerBoard.placeShip(
+              ship,
+              pickedOrientation,
+              Math.floor(
+                Math.floor(Math.random() * 9 - ship.lengthShip) +
+                  ship.lengthShip,
+              ),
+              Math.floor(Math.floor(Math.random() * 9)),
+            );
+          }
+        }
+      });
+    },
   };
 }
