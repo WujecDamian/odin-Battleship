335d5c8f40ba53d95e5ef06c26dec3822c4c89ae
WujecDamian <wujecdamian12@gmail.com>
2026-04-09 21:40:25 +0200

    feat: add call to attackShip for both players in UI

--- src/scripts/DOM/ui.js
+++ src/scripts/DOM/ui.js
@@ -4,11 +4,15 @@ import { aiPlayer } from "../player/aiPlayer.js";
 import { renderBoard } from "./renderBoard.js";
 import { placePcShips } from "./placePcShips.js";
 import { placeAiShips } from "./placeAiShips.js";
+import { playTurn } from "./playTurn.js";
+import { attackShip } from "./attackShip.js";
 
 let newGameBtn1 = document.querySelector(".newgame__btn1");
 let newGameBtn2 = document.querySelector(".newgame__btn2");
 let startGameBtn = document.querySelector(".startgame__btn");
 let reloadGameBtn = document.querySelector(".reloadgame__btn");
+let board1 = document.querySelector(".board1");
+let board2 = document.querySelector(".board2");
 
 reloadGameBtn.addEventListener("click", () => {
   window.location.reload();
@@ -17,8 +21,16 @@ startGameBtn.addEventListener("click", () => {
   /* player 1 */
   let player1 = realPlayer();
   placePcShips(player1);
+  attackShip(player1, board1);
   /* player 2 */
   let player2 = aiPlayer();
   placeAiShips(player2);
-  /*  */
+  attackShip(player2, board2);
+
+  /*  
+  Turns function.
+    - human player starts
+    - turn changes on clicking square (of course if square is empty)
+  */
+  playTurn();
 });
