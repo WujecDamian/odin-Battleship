ee8186a564019e3d1e5211f42739d748c6aa1477
WujecDamian <wujecdamian12@gmail.com>
2026-04-09 21:39:52 +0200

    refactor: remove attacking functionality from placePcShips function

--- src/scripts/DOM/placePcShips.js
+++ src/scripts/DOM/placePcShips.js
@@ -2,34 +2,12 @@ import { renderBoard } from "./renderBoard.js";
 
 export function placePcShips(player) {
   let board1 = document.querySelector(".board1");
-
   let playerboard = player.playerBoard.board;
   player.placeShips(player.playerBoard.ships);
-
   renderBoard(playerboard, "board1");
-
-  board1.addEventListener("click", (event) => {
-    const target = event.target;
-    if (target.className === "square") {
-      if (target.innerHTML !== "o" && target.innerHTML !== "x") {
-        console.log(`board 1: ${target.id}`);
-
-        let id = target.id.split(",");
-
-        player.playerBoard.receiveAttack(id[0], id[1]);
-
-        let attack = player.playerBoard.receiveAttack(id[0], id[1]);
-        if (attack === true) {
-          target.innerHTML = "o";
-        } else {
-          target.innerHTML = "x";
-        }
-      }
-    }
-  });
+  /* Showing where human player ships are placed */
   let fieldsWithShips = player.playerBoard.fieldsWithShips;
   let squares = document.querySelectorAll(".board1 > .square");
-
   fieldsWithShips.forEach((field) => {
     for (let i = 0; i < 100; i++) {
       if (squares[i].id === field) {
